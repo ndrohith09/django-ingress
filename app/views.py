d4c0672c6602ae367f2234380538fd669ce8440b
ndrohith09 <ndrohith09@gmail.com>
2023-02-24 22:15:57 +0530

    ingress

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import os
 from django.http import HttpResponse
 # Create your views here.
 
@@ -8,7 +9,15 @@ def home(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     print('ip address of the client = ' , ip)
-    return HttpResponse('<h1>Hello World . ip address of the client = ' + ip + '</h1>')
+    pod_name = os.environ.get('HOSTNAME')
+    pod_ip = os.environ.get('POD_IP')
+
+
+    return HttpResponse(
+        '<h1>Hello World . ip address of the client = ' + ip + '</h1>'
+        '<h1>Hello World . pod name = ' + pod_name + '</h1>'
+        '<h1>Hello World . pod ip = ' + pod_ip + '</h1>'
+        )
 
 def page(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -17,5 +26,12 @@ def page(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     print('ip address of the client = ' , ip)
+    pod_name = os.environ.get('HOSTNAME')
+    pod_ip = os.environ.get('POD_IP')
 
-    return HttpResponse('<h1>Page 2. ip address of the client = ' + ip + '</h1>')
+
+    return HttpResponse(
+        '<h1>Page 2 . ip address of the client = ' + ip + '</h1>'
+        '<h1>Page 2 . pod name = ' + pod_name + '</h1>'
+        '<h1>Page 2 . pod ip = ' + pod_ip + '</h1>'
+        )
