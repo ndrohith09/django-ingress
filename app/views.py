ad5b9bd397a1b35ba3819a2feb2c74a4239864ab
ndrohith09 <ndrohith09@gmail.com>
2023-02-25 00:09:35 +0530

    ingress

--- app/views.py
+++ app/views.py
@@ -9,14 +9,12 @@ def home(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     print('ip address of the client = ' , ip)
-    pod_name = os.environ.get('HOSTNAME')
-    pod_ip = os.environ.get('POD_IP')
+    pod_name = os.environ.get('HOSTNAME') 
 
 
     return HttpResponse(
-        '<h1>Hello World . ip address of the client = ' + ip + '</h1>'
-        '<h1>Hello World . pod name = ' + pod_name + '</h1>'
-        '<h1>Hello World . pod ip = ' + pod_ip + '</h1>'
+        '<h1>Hello World . ip address of the client = ' + str(ip) + '</h1>'
+        '<h1>Hello World . pod name = ' + str(pod_name) + '</h1>' 
         )
 
 def page(request):
@@ -26,12 +24,10 @@ def page(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     print('ip address of the client = ' , ip)
-    pod_name = os.environ.get('HOSTNAME')
-    pod_ip = os.environ.get('POD_IP')
+    pod_name = os.environ.get('HOSTNAME') 
 
 
     return HttpResponse(
-        '<h1>Page 2 . ip address of the client = ' + ip + '</h1>'
-        '<h1>Page 2 . pod name = ' + pod_name + '</h1>'
-        '<h1>Page 2 . pod ip = ' + pod_ip + '</h1>'
+        '<h1>Hello World . ip address of the client = ' + str(ip) + '</h1>'
+        '<h1>Hello World . pod name = ' + str(pod_name) + '</h1>' 
         )
